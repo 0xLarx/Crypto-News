a2c9775fbe6d384591df5961b792f8e1b3da0026
Larx-NFT <sametcantasgiran@outlook.com>
2023-06-20 19:05:33 +0300

    Initial Commit

---
+++ Crypto Newsletter/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Crypto Newsletter
+//
+//  Created by Sametcan Taşgıran on 20.06.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
